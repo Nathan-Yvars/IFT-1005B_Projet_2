5496c6534443c42d8c7681bc9333f0cee62c0922
Nathan-Yvars <natyvars@gmail.com>
2024-12-05 20:59:39 -0500

    Ajout de la l'outil de conversion

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const conversion = {
+    cm : 100,
+    km : 0.001,
+    pied : 3.28084,
+    pouce : 39.3701
+};
+
+function Convert(){
+    const val = parseFloat(document.getElementById('valeur').value);
+    const startunit = document.getElementById("startunit").value;
+    const convertunit = document.getElementById("convertunit").value;
+
+    console.log(val);
+    console.log(startunit);
+    console.log(convertunit);
+
+    if (isNaN(val) || val < 0){
+        document.getElementById("convertresult").textContent = 'Erreur';
+        return;
+    }
+
+    const newval = val * conversion[convertunit];
+
+    document.getElementById("convertresult").textContent = newval;
+ };
+
+document.getElementById("convertbutton").addEventListener("click", Convert);
+
